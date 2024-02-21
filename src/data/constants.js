3fd8b725d8bc916e66e1b0596899484cc50750ad
tausif40 <tausif6040@gmail.com>
2024-02-21 09:26:50 +0530

    fist commit

---
+++ src/data/constants.js
@@ -0,0 +1,153 @@
+export const Bio = {
+  name: "Mohd. Tausif",
+  roles: [
+    "Frontend Developer", "React Developer"
+  ],
+  description:
+    "Recently graduated with a degree in computer Science and a good knowledge of frontend development. I'm good at using HTML, CSS, and JavaScript and I always keep up with the newest trends in the industry. A quick learner with excellent problem-solving abilities and a collaborative mindset. Looking forward to starting my career in making websites look awesome, bringing new ideas and lots of excitement to create cool experiences.",
+  github: "https://github.com/tausif40",
+  resume:
+    "https://drive.google.com/file/d/1cvWhrcRQzasx1qpTRoiU762BrZhdVWIP/view?usp=drivesdk",
+  linkedin: "https://www.linkedin.com/in/mohd-tausif-0142692a9/",
+  insta: "https://www.instagram.com/tausif_778?igsh=MzFraDJvdGo3dXVw",
+};
+
+export const skills = [
+  {
+    title: "Languages",
+    skills: [
+      {
+        name: "C Language",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/5/5e/C2ndEdition.svg",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/32/C%2B%2B_logo.png",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "tailwind css",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      // {
+      //   name: "Git",
+      //   image:
+      //     "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      // },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "https://gujaratisciencecollege.in/wp-content/uploads/2022/05/PMBGSC-LOGO-web-White.png",
+    school: "P.M.B. Gujarati science college",
+    date: "Aug 2020 - July 2023",
+    desc: "I have completed my Bachelor's degree in Computer Science from PMB Gujarati science collage, Indore. I have learned in C/C++, Data Structures, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others in college.",
+    degree: "BCA - (Bachelor in computer application)",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/BSEB_LOGO.svg/800px-BSEB_LOGO.svg.png",
+    school: "S. M. Intermediate college, Bihar (Hajipur), ",
+    date: "July 2017 - March 2019",
+    desc: "I completed my class 12th high school education at S. M. Intermediate college, Bihar (Hajipur), where I studied Science.",
+    degree: "BSEB(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/BSEB_LOGO.svg/800px-BSEB_LOGO.svg.png",
+    school: "Rajkiyakrit Higher School, Bihar (Hajipur), ",
+    date: "Apr 2017 - Apr 2019",
+    desc: "I completed my class 10th education at Rajkiyakrit Higher School, Hajipur, Bihar.",
+    degree: "BSEB(X), All Subject",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "e-store",
+    description:
+      "Easy Navigation to Explore effortlessly with simple clicks to find what you want. Add to Cart & Wishlist option to Save for later with 'Add to Cart' and build your Wishlist for future buys. Search Functionality to Find what you need in seconds for our search functionality. Product Filters to Customize your search with more option, making it simple to find exactly what you need. Product Management Easily add, update, delete your product and Admin can add category and Subcategory.",
+    image:
+      "https://i.postimg.cc/fTdfyMwS/e-store-screenshort.png",
+    tags: [ "HTML", "Css", "Javascript", "React Js" ],
+    category: "web app",
+    github: "https://github.com/tausif40/e-store.git",
+    webapp: "",
+  },
+  {
+    id: 1,
+    title: "BIDME",
+    description:
+      "This is a simple bidding web app built with the popular JavaScript library React. In this web application user can use online auction and bidding facilities to sell their products.",
+    image:
+      "https://i.postimg.cc/66k1YjqR/Whats-App-Image-2024-02-18-at-1-30-54-AM.jpg",
+    tags: [ "HTML", "Css", "Javascript", "React Js" ],
+    category: "web app",
+    github: "https://github.com/tausif40/BIDME--React-ui.git",
+    webapp: "",
+  }
+];
+
